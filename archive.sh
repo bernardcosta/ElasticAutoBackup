fdaa3b97846c5d6ba779d48a5aa116aa162d19e2
Bernard Costa <costa.ber95@gmail.com>
2022-02-07 21:14:27 +0100

    removing global variables from funciton parameters

--- archive.sh
+++ archive.sh
@@ -12,6 +12,7 @@ Usage: ${0##*/} [options] [-iopcft]
    -c <index name>               Custom archive for only this index
    -f <date from> ex:2021.01.12  date from when to start archiving
    -t <date to>   ex:2021.01.12  date to stop archiving at
+   -d                            to delete indices once transfered to archive
 
 EOF
 }
@@ -26,31 +27,45 @@ check_date() {
 }
 
 check_date_range() {
-  if [ $(date -f "%Y.%m.%d" -j $1 +"%s") -ge $(date -f "%Y.%m.%d" -j $2 +"%s") ]
+  if [ $(date -f "%Y.%m.%d" -j ${FROM_DATE} +"%s") -ge $(date -f "%Y.%m.%d" -j ${TO_DATE} +"%s") ]
   then
-    echo "\"From\" date ($1) cannot be more recent than \"To\" date ($2)"
+    echo "\"From\" date (${FROM_DATE}) cannot be more recent than \"To\" date (${TO_DATE})"
     exit 1;
   fi
 }
 
 dump() {
   limit=1800
-
-  if [ "$#" -eq 4 ]
+  if [ "$#" -eq 1 ]
     then
-      limit="$4"
+      limit="$1"
     fi
 
   elasticdump \
-        --input=http://$1/$3 \
-        --output=http://$2/$3 \
+        --input=http://${INPUT_SERVER}/${INDEX} \
+        --output=http://${OUTPUT_SERVER}/${INDEX} \
         --type=mapping\
         --limit=$limit
   elasticdump \
-        --input=http://$1/$3 \
-        --output=http://$2/$3 \
+        --input=http://${INPUT_SERVER}/${INDEX} \
+        --output=http://${OUTPUT_SERVER}/${INDEX} \
         --type=data\
         --limit=$limit
+}
+
+delete_index(){
+  original_size=$( curl -s -XGET "$INPUT_SERVER/$INDEX/_stats" | jq '._all.primaries.docs.count' )
+  archived_size=$( curl -s -XGET "$OUTPUT_SERVER/$INDEX/_stats" | jq '._all.primaries.docs.count' )
+
+  if [ $original_size = $archived_size ]
+  then
+    echo "Deleting Index from Original server"
+    curl -XDELETE "$INPUT_SERVER/$INDEX"
+  else
+    echo "Warning: Not deleting Index! Backup documents do not match original."
+    echo "    - Original docs: $original_size"
+    echo "    - Archived docs: $archived_size"
+  fi
 }
 
 # export .env files
@@ -59,13 +74,14 @@ export $(grep -v '^#' .env | xargs)
 INPUT_SERVER=${DEFAULT}
 OUTPUT_SERVER=${ARCHIVE}
 PORT_FORWARD=false
+DELETE=false
 FROM_DATE=$(date -j -v-14d +"%Y.%m.%d" )
 TO_DATE=$(date -j -v-7d +"%Y.%m.%d" )
 INDEX=""
 OPTIND=1
 # Resetting OPTIND is necessary if getopts was used previously in the script.
 # It is a good idea to make OPTIND local if you process options in a function.
-while getopts "hi:o:pc:f:t:" opt; do
+while getopts "hi:o:pc:f:t:d" opt; do
        case $opt in
            h) usage
               exit 0 ;;
@@ -78,14 +94,16 @@ while getopts "hi:o:pc:f:t:" opt; do
                   check_date $FROM_DATE ;;
            t)  TO_DATE=$OPTARG
                   check_date $TO_DATE;;
+           d)  DELETE=true;;
            *)  usage >&2
                exit 1 ;;
        esac
    done
-check_date_range ${FROM_DATE} ${TO_DATE}
+check_date_range
 echo "Archiving dates from: $FROM_DATE to $TO_DATE"
 
-if ${PORT_FORWARD} ; then
+if ${PORT_FORWARD}
+ then
    # kill ssh tunnel already using this port
    port1=$( ps aux | grep '[9]201:' | awk '{print $2}' )
    port2=$( ps aux | grep '[9]202:' | awk '{print $2}' )
@@ -118,19 +136,10 @@ echo "                   to: $OUTPUT_SERVER"
 
 if  ! [ -z "$INDEX" ]
   then
+    dump
 
-    dump ${INPUT_SERVER} ${OUTPUT_SERVER} ${INDEX}
-
-    original_size=$( curl -s -XGET "$INPUT_SERVER/$INDEX/_stats" | jq '._all.primaries.docs.count' )
-    archived_size=$( curl -s -XGET "$OUTPUT_SERVER/$INDEX/_stats" | jq '._all.primaries.docs.count' )
-
-    if [ $original_size = $archived_size ]
+    if ${DELETE}
     then
-      echo "Deleting Index from Original server"
-      curl -XDELETE "$INPUT_SERVER/$INDEX"
-    else
-      echo "Warning: Not deleting Index! Backup documents do not match original."
-      echo "    - Original docs: $original_size"
-      echo "    - Archived docs: $archived_size"
+      delete
     fi
   fi
